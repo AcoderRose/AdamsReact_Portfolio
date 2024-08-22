44e9a0600cbdb43fb0eec637b273311cfdaad416
AcoderRose <addyro96@att.net>
2024-08-22 13:57:07 -0700

    Added comments to Navigation.jsx and added project component with card layout for portfolio projects

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,6 +2,7 @@ import { Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import "../styles/style.css";
 
+// Navigation items with their corresponding paths and labels
 const navigationItems = [
   { path: "/About", label: "About Me" },
   { path: "/Contact", label: "Contact" },
@@ -9,6 +10,7 @@ const navigationItems = [
   { path: "/Resume", label: "Resume" },
 ];
 
+// Navigation component
 function Navigation() {
   return (
     <Nav variant="tabs" defaultActiveKey="/">
